e529c6c26c8a7a0a8660587d2d5169e20111f8c9
Nguyễn Văn Thành <thanh.nguyenvan@haravan.com>
2021-01-03 21:02:40 +0700

    up 3

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app ahihi!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
