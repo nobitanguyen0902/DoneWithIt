b5f66d51ca4b092efc34a028fed7a10f72918bd0
Nguyễn Văn Thành <thanh.nguyenvan@haravan.com>
2021-01-05 00:32:16 +0700

    up

---
+++ src/layouts/layout/Menu.tsx
@@ -0,0 +1,8 @@
+import * as  React from 'react';
+import { View, Text } from 'react-native';
+
+export const Menu = React.memo(() => {
+    return <View style={{ flex: 1, marginTop: 114, padding: 8 }}>
+        <Text>Menu</Text>
+    </View>
+})
--- src/layouts/layout/index.tsx
+++ src/layouts/layout/index.tsx
@@ -3,7 +3,9 @@ import { SafeAreaView, StyleSheet, View } from "react-native";
 import Constants from 'expo-constants';
 
 export const Layout = React.memo((props) => {
-    return <SafeAreaView style={styles.container}>{props.children}</SafeAreaView>
+    return <SafeAreaView style={styles.container}>
+        <View>{props.children}</View>
+    </SafeAreaView>
 })
 
 const styles = StyleSheet.create({
