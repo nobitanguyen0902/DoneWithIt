9cbe8a31bd62310c463f7fcc2fc396da9c3daad0
Nguyễn Văn Thành <thanh.nguyenvan@haravan.com>
2021-01-05 00:37:24 +0700

    up

--- src/layouts/layout/Menu.tsx
+++ src/layouts/layout/Menu.tsx
@@ -1,8 +1,14 @@
 import * as  React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export const Menu = React.memo(() => {
     return <View style={{ flex: 1, marginTop: 114, padding: 8 }}>
         <Text>Menu</Text>
     </View>
 })
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+});
